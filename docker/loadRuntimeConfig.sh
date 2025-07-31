d74ccacf4fe2ef1b73f6a914e09369b33d4edcfb
Kalin Krustev <kalin.krustev@gmail.com>
2025-07-31 23:43:35 +1000

    fix: support read only root filesystem (#43)

--- docker/loadRuntimeConfig.sh
+++ docker/loadRuntimeConfig.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
-sed -i 's#__CENTRAL_LEDGER_ENDPOINT__#'"$CENTRAL_LEDGER_ENDPOINT"'#g' /usr/share/nginx/html/runtime-env.js
+#! /bin/sh
+if grep -q "__CENTRAL_LEDGER_ENDPOINT__" /usr/share/nginx/html/runtime-env.js; then
+  sed -i 's#__CENTRAL_LEDGER_ENDPOINT__#'"$CENTRAL_LEDGER_ENDPOINT"'#g' /usr/share/nginx/html/runtime-env.js;
+else
+  echo "skipping replacement.";
+fi
 
 exec "$@"
